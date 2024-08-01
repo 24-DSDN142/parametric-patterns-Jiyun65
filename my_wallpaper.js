fdea3c5f68c03ad790a0f34d968a69d24b68511c
Jiyun65 <anjiyu@myvuw.ac.nz>
2024-08-01 15:20:26 +1200

    Changing name

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -5,9 +5,9 @@ let rect_height = 10;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(false); //set this to false when you're ready to print
+  pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 400;
@@ -49,24 +49,23 @@ function drawPixelArtCat(x, y, scale) {
     [0, 0, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0],
   ];
 
-  let orangeColor = color(255, 211, 122); // for the cat
-  let whiteColor = color(255, 255, 255); // for highlights
-   let blackColor = color(0, 0, 0); //  for the outline 
-   let greyColor = color(173, 87, 129); // Grey color for the shadow
+  let catColor = color(255, 211, 122); // for the cat
+  let highlights = color(255, 255, 255); // for highlights
+   let outline = color(0, 0, 0); //  for the outline 
+ 
   
   for (let i = 0; i < catPattern.length; i++) {
     for (let j = 0; j < catPattern[i].length; j++) {
       if (catPattern[i][j] === 1) {
-        fill(orangeColor);
+        fill(catColor);
         
       } else if (catPattern[i][j] === 2) {
-        fill(whiteColor);
+        fill(highlights);
   
       } else if (catPattern[i][j] === 3) {
-        fill(blackColor);
+        fill(outline);
 
-      } else if (catPattern[i][j] === 4) {
-        fill(greyColor);
+    
       } else {
         continue;  
       }
@@ -95,18 +94,18 @@ function drawPixelArtHeart(x, y, scale) {
     [0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
 
-  let pinkColor = color(255, 189, 227); // for the heart
-  let whiteColor = color(255, 255, 255); // for highlights
-  let blackColor = color(0, 0, 0); // for the outline 
+  let heartColor = color(255, 189, 227); // for the heart
+  let highlights = color(255, 255, 255); // for highlights
+  let outline = color(0, 0, 0); // for the outline 
 
   for (let i = 0; i < heartPattern.length; i++) {
     for (let j = 0; j < heartPattern[i].length; j++) {
       if (heartPattern[i][j] === 1) {
-        fill(pinkColor);
+        fill(heartColor);
       } else if (heartPattern[i][j] === 2) {
-        fill(whiteColor);
+        fill(highlights);
       } else if (heartPattern[i][j] === 3) {
-        fill(blackColor);
+        fill(outline);
       } else {
         continue;
       }
@@ -136,18 +135,18 @@ function drawPixelArtDog(x, y, scale) {
     
     
  ];
-  let blueColor = color(171, 207, 255); // for the Dog
-  let whiteColor = color(255, 255, 255); // for highlights
-  let blackColor = color(0, 0, 0); // for the outline 
+  let dogColor = color(171, 207, 255); // for the Dog
+  let highlights = color(255, 255, 255); // for highlights
+  let outline = color(0, 0, 0); // for the outline 
 
   for (let i = 0; i < dogPattern.length; i++) {
     for (let j = 0; j < dogPattern[i].length; j++) {
       if (dogPattern[i][j] === 1) {
-        fill(blueColor);
+        fill(dogColor);
       } else if (dogPattern[i][j] === 2) {
-        fill(whiteColor);
+        fill(highlights);
       } else if (dogPattern[i][j] === 3) {
-        fill(blackColor);
+        fill(outline);
       } else {
         continue;
       }
@@ -174,18 +173,18 @@ function drawPixelArtStar(x, y, scale) {
     [0, 0, 3, 1, 3, 3, 0, 0, 0, 3, 3, 1, 3, 0, 0],
     [0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0],
   ];
-  let mintColor = color(184, 252, 226); // for the Star
-  let whiteColor = color(255, 255, 255); // for highlights
-  let blackColor = color(0, 0, 0); // for the outline 
+  let starColor = color(184, 252, 226); // for the Star
+  let highlights = color(255, 255, 255); // for highlights
+  let outline = color(0, 0, 0); // for the outline 
 
   for (let i = 0; i < starPattern.length; i++) {
     for (let j = 0; j < starPattern[i].length; j++) {
       if (starPattern[i][j] === 1) {
-        fill(mintColor);
+        fill(starColor);
       } else if (starPattern[i][j] === 2) {
-        fill(whiteColor);
+        fill(highlights);
       } else if (starPattern[i][j] === 3) {
-        fill(blackColor);
+        fill(outline);
       } else {
         continue;
       }
@@ -213,18 +212,18 @@ function drawPixelArtHeart2(x, y, scale) {
     [0, 0, 0, 0, 0, 0, 0, 3, 1, 3, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
-  let yellowColor = color(244, 255, 186); // for the Heart2
-  let whiteColor = color(255, 255, 255); // for highlights
-  let blackColor = color(0, 0, 0); // for the outline 
+  let heartwColor = color(244, 255, 186); // for the Heart2
+  let highlights = color(255, 255, 255); // for highlights
+  let outline = color(0, 0, 0); // for the outline 
 
   for (let i = 0; i < heart2Pattern.length; i++) {
     for (let j = 0; j < heart2Pattern[i].length; j++) {
       if (heart2Pattern[i][j] === 1) {
-        fill(yellowColor);
+        fill(heart2Color);
       } else if (heart2Pattern[i][j] === 2) {
-        fill(whiteColor);
+        fill(highlights);
       } else if (heart2Pattern[i][j] === 3) {
-        fill(blackColor);
+        fill(outline);
       } else {
         continue;
       }
